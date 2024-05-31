57671cf30793a4b0ac89a904402e39afb14f3427
JazHyc <feronest@yahoo.com>
2024-05-31 12:09:23 +0200

    Habrok fine tuning

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Tensorboard: tensorboard --logdir [path]
 
 ## IMPALA (CBET)
 
+In cbet directory
 Usage: OMP_NUM_THREADS=1 python main.py --model cbet --env MiniGrid-Unlock-v0 --intrinsic_reward_coef=0.005 --total_frames 1000000 --num_actors 24 --savedir ../logs/impala --xpid unlock-tabula-rasa-1M-ego
 
 OMP_NUM_THREADS=1 python main.py --model cbet --env CrafterReward-v1 --intrinsic_reward_coef=0.005 --total_frames 1000000 --num_actors 24 --savedir ../logs/impala --xpid unlock-tabula-rasa-1M-ego
--- cbet/hpc.sh
+++ cbet/hpc.sh
@@ -1,2 +1,17 @@
-pip install pip==21.0
-module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=5:00:00
+#SBATCG --partition=gpu
+#SBATCH --gpus-per-node=1
+#SBATCH --mem=10gb
+#SBATCH --job-name=minigrid-hyperparam-search
+
+#SBATCH --array=0-2
+coeff_strength=(0.001 0.0025 0.005)
+
+module load Python/3.8.16-GCCcore-11.2.0 
+module load CUDA/12.1.1
+source $HOME/venvs/cbet/bin/activate
+OMP_NUM_THREADS=1 python main.py --model cbet --env MiniGrid-Unlock-v0 --intrinsic_reward_coef=${coeff_strength[${SLURM_ARRAY_TASK_ID}]} --total_frames 1000000 --num_actors 8 --savedir ../logs/impala --xpid unlock-tabula-rasa-1M-coeff-${coeff_strength[${SLURM_ARRAY_TASK_ID}]}
