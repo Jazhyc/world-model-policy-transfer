d9e5498f98192751a64cfb0e1d0e3f47a982ed75
JazHyc <feronest@yahoo.com>
2024-08-31 23:24:17 +0200

    Unstable hpc results

--- cbet/hpc.sh
+++ cbet/hpc.sh
@@ -2,12 +2,12 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --time=4:00:00
+#SBATCH --time=3:00:00
 #SBATCG --partition=gpu
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=20gb
-#SBATCH --job-name=minigrid-stress-impala
-## SBATCH --array=0-2
+#SBATCH --job-name=i-c-b
+#SBATCH --array=0-4
 #coeff_strength=(0.001 0.0025 0.005)
 
 module load Python/3.8.16-GCCcore-11.2.0 
@@ -15,7 +15,7 @@ module load CUDA/12.1.1
 source $HOME/venvs/cbet/bin/activate
 
 # Regular
-# OMP_NUM_THREADS=1 python main.py --model cbet --env CrafterReward-v0 --intrinsic_reward_coef=0.0 --total_frames 2000000 --num_actors 8 --savedir ../logs/impala --xpid crafter-base-2M
+OMP_NUM_THREADS=1 python main.py --model cbet --env CrafterReward-v0 --intrinsic_reward_coef=0.0 --total_frames 1000000 --num_actors 8 --savedir ../logs/impala --xpid Crafter-base-sweep-${SLURM_ARRAY_TASK_ID} --seed ${SLURM_ARRAY_TASK_ID} 
 
 # Pre-train minigrid Doorkey
 # OMP_NUM_THREADS=1 python main.py --model cbet --env MiniGrid-DoorKey-8x8-v0 --intrinsic_reward_coef=0.0025 --total_frames 1000000 --num_actors 8 --savedir ../logs/impala --xpid minigrid-pretrain-1M-Doorkey8x8-2 --no_reward
@@ -24,7 +24,7 @@ source $HOME/venvs/cbet/bin/activate
 # OMP_NUM_THREADS=1 python main.py --model cbet --env MiniGrid-Unlock-v0 --intrinsic_reward_coef=0.0 --total_frames 1000000 --num_actors 8 --savedir ../logs/impala --xpid minigrid-transfer-1M-unlock --checkpoint=../logs/impala/minigrid-pretrain-1M-Doorkey8x8/model.tar
 
 # Pre-train crafter cbet
-OMP_NUM_THREADS=1 python main.py --model cbet --env CrafterReward-v0 --intrinsic_reward_coef=0.0025 --total_frames 1000000 --num_actors 8 --savedir ../logs/impala --xpid crafter-pretrain-1M-2 --seed 42 --no_reward
+# OMP_NUM_THREADS=1 python main.py --model cbet --env CrafterReward-v0 --intrinsic_reward_coef=0.0025 --total_frames 1000000 --num_actors 8 --savedir ../logs/impala --xpid crafter-pretrain-1M-2 --seed 42 --no_reward
 
 # Transfer crafter cbet
 # OMP_NUM_THREADS=1 python main.py --model cbet --env CrafterReward-v0 --intrinsic_reward_coef=0.0 --total_frames 1000000 --num_actors 8 --savedir ../logs/impala --xpid crafter-transfer-1M --checkpoint=../logs/impala/crafter-pretrain-1M/model.tar
--- dreamerv3/hpc.sh
+++ dreamerv3/hpc.sh
@@ -2,12 +2,12 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCG --partition=gpu
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=32gb
-#SBATCH --job-name=crafter-pre-train-backup
-## SBATCH --array=0-1
+#SBATCH --job-name=d-m-c-sweep
+#SBATCH --array=0-4
 # plan_ratio=(256 1024)
 
 module load Python/3.10.8-GCCcore-12.2.0
@@ -23,10 +23,10 @@ cd ..
 # python -m dreamerv3 --configs minigrid-transfer --logdir ./logs/dreamerv3/minigrid-transfer-1M-Unlock --run.from_checkpoint ./logs/dreamerv3/minigrid-pre-train-1M-Doorkey8x8/checkpoint.ckpt
 
 # Crafter Tabula Rasa / Base (change intrinsic and path)
-# python -m dreamerv3 --configs crafter-cbet --logdir ./logs/dreamerv3/minigrid-base --run.intrinsic False
+python -m dreamerv3 --configs minigrid-unlock --logdir ./logs/dreamerv3/minigrid-cbet-sweep-${SLURM_ARRAY_TASK_ID} --run.intrinsic True --seed ${SLURM_ARRAY_TASK_ID}
 
 # Crafter Pre-train
-python -m dreamerv3 --configs crafter-pre-train --logdir ./logs/dreamerv3/crafter-pre-train-1M-2
+# python -m dreamerv3 --configs crafter-pre-train --logdir ./logs/dreamerv3/crafter-pre-train-1M-2
 
 # Crafter Transfer
 # python -m dreamerv3 --configs crafter-transfer --logdir ./logs/dreamerv3/crafter-transfer-1M --run.from_checkpoint ./logs/dreamerv3/crafter-pre-train-1M/checkpoint.ckpt
